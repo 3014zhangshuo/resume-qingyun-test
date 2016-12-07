c6d3877300d6c499fca17e388a9ae600e8ca6b46
xbthh <hanhan_smith@hotmail.com>
2016-12-08 04:47:44 +0800

    可以生成pdf文件

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,8 +7,18 @@ class WelcomeController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: "welcome.pdf"   # Excluding ".pdf" extension.
+        render pdf: "welcome.pdf",
+        template: "welcome/show.pdf.erb"
       end
     end
+  end
+
+  def download
+    html = render_to_string(:action => :show)
+  pdf = WickedPdf.new.pdf_from_string(html)
+
+  send_data(pdf,
+    :filename => "welcome.pdf",
+    :disposition => 'attachment')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  root 'welcome#show'
+
+  resources :welcomes do
+  end
+
+  get 'welcome/pdf' => 'welcome#download'
 end
