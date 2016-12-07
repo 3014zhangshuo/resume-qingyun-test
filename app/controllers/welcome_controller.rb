f6465a7662646691f450ee13a32cee5c5a6343fa
xbthh <hanhan_smith@hotmail.com>
2016-12-08 03:47:11 +0800

    生成show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,13 @@ class WelcomeController < ApplicationController
   def index
     flash[:notice] = "早安！你好！"
   end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "welcome.pdf"   # Excluding ".pdf" extension.
+      end
+    end
+  end
 end
