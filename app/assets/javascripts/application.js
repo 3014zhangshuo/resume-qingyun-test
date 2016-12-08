37d792a4e180f3eec42418d1fe2528b72b8cc182
cndaniel <tigerljt@gmail.com>
2016-12-08 08:56:15 +0800

    fix application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,5 +42,18 @@
 //= require plugins/table.min.js
 //= require plugins/url.min.js
 //= require plugins/video.min.js
+/*
+ *= require plugins/char_counter.min.css
+ *= require plugins/code_view.min.css
+ *= require plugins/colors.min.css
+ *= require plugins/emoticons.min.css
+ *= require plugins/file.min.css
+ *= require plugins/fullscreen.min.css
+ *= require plugins/image_manager.min.css
+ *= require plugins/image.min.css
+ *= require plugins/line_breaker.min.css
+ *= require plugins/quick_insert.min.css
+ *= require plugins/table.min.css
+ *= require plugins/video.min.css
 
 //= require_tree .
