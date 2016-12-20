31f6f0c68cec6c41a83f5df1300568f007ef52e2
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-12-20 18:38:02 +0800

    拉下最新的分支

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218105638) do
+ActiveRecord::Schema.define(version: 20161219073139) do
 
   create_table "resumes", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20161218105638) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "occupation"
   end
 
   create_table "users", force: :cascade do |t|
