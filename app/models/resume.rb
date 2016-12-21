0cfc63e7cdd175f8e3e6450062bb2cdc2d1e2f9a
dyz2102 <dyz2102@gmail.com>
2016-12-22 00:23:52 +0800

    Revert "96"

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,2 @@
 class Resume < ApplicationRecord
-  mount_uploader :past_project_image1, ImageUploader
-  mount_uploader :past_project_image2, ImageUploader
-  mount_uploader :past_project_image3, ImageUploader
-  belongs_to :user, foreign_key: :user_id
 end
--- db/migrate/20161221034922_add_user_id_to_resume.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToResume < ActiveRecord::Migration[5.0]
-  def change
-    add_column :resumes, :user_id, :integer
-  end
-end
