92f98eb26344cf036550c9ec644730a1b8ab0b62
David <dyz2102@gmail.com>
2016-12-22 17:10:52 +0800

    changed welcomes from resources to namespace and added get :entry route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :welcomes do
+  namespace :welcomes do
+		get :entry
   end
 
   get 'user/resumes/pdf' => 'user/resumes#download'
