ddd8072b0984b5e909ac88a495d7d9187de11a82
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-20 23:04:19 +0800

    create a admin resume controller

---
+++ app/controllers/admin/resumes_controller.rb
@@ -0,0 +1,13 @@
+class Admin::ResumesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @user = User.find(params[:id])
+    @resumes = @user.resumes
+  end
+
+  def show
+    @resume = Resume.find(params[:id])
+  end
+end
