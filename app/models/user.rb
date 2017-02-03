370100da7890463fc1c83a096494f62ae86bddc1
3014zhangshuo <3014zhangshuo@gmail.com>
2017-02-03 19:14:13 +0800

    user sign up can create the user_code

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,16 @@
 #
 
 class User < ApplicationRecord
-  before_create :user_code
+  #before_create :create_user_code
+  before_update :create_user_code
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
 	has_many :resumes
-
+  #attr_accessor :user_code
+  #validate :create_user_code, :on => :create
 	def admin?
 	  is_admin
 	end
@@ -43,9 +45,10 @@ class User < ApplicationRecord
 
   private
 
-  def user_code
+  def create_user_code
     if self.user_code.blank?
        self.user_code = SecureRandom.hex
+       self.save!
     end
   end
 
