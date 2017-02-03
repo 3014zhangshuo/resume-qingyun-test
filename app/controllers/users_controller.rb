c93ff0fc09fe6c70271756de5d27fea205e3cd7d
3014zhangshuo <3014zhangshuo@gmail.com>
2017-02-03 21:38:26 +0800

    fixed some bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :confirmed_to_current_user_resumes
+  #before_action :confirmed_to_current_user_resumes
   before_action :inspect_user_is_confrim
   #验证用户是否通过验证
   def edit
@@ -20,12 +20,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:confirm_code)
   end
 
-  def confirmed_to_current_user_resumes
-    if @user = User.find(params[:id])
-      @user.is_confirmed == true
-      redirect_to root_path
-      flash[:notice] = "不能进入"
-    end
-  end
+  # def confirmed_to_current_user_resumes
+  #   if @user = User.find(params[:id])
+  #     @user.is_confirmed == true
+  #     redirect_to root_path
+  #     flash[:notice] = "不能进入"
+  #   end
+  # end
 
 end
