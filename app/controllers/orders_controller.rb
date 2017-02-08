494a9022898a97df3f71a4f5d212db602204a025
3014zhangshuo <3014zhangshuo@gmail.com>
2017-02-08 16:07:18 +0800

    change new

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,19 +9,21 @@ class OrdersController < ApplicationController
     @order.resume = @resume
     @order.pay_code = SecureRandom.hex
     @order.save
-    redirect_to choice_order_path(@order)
+    #binding.pry
+    redirect_to choice_order_path(@order.token)
   end
 
   def choice
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
+    #binding.pry
   end
 
   def choice_submit
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
     @order.update(order_params)
     if @order.save!
       @order.order_choosed!
-      redirect_to pay_order_path(@order)
+      redirect_to pay_order_path(@order.token)
       flash[:notice] = "订单创建成功请支付"
     else
       render :new
@@ -29,12 +31,12 @@ class OrdersController < ApplicationController
   end
 
   def pay
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
     #@resume = @order.resume
   end
 
   def pay_submit
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
     @resume = @order.resume
     @order.update(order_params)
     @order.save!
@@ -47,7 +49,7 @@ class OrdersController < ApplicationController
       redirect_to user_resume_editor_path(@resume)
     else
       flash[:warning] = "请输入正确的验证码"
-      redirect_to pay_order_path(@order)
+      redirect_to pay_order_path(@order.token)
     end
   end
 
